819964e194490fdd84ad5d5be2817782a672fd10
ValentinDuprat <vrduprat@ucsc.edu>
2025-04-02 17:50:28 -0700

    valdescription
    Name, pronouns, and 2 sentence description.

---
+++ Session1Prompt.py
@@ -0,0 +1,13 @@
+#This program will state my name, pronouns and a short description of me
+
+#This function will print the above
+def valdescription():
+	print("Hello, my name is Valentin Duprat and I go by he/him. My favorite movie is Shawshank Redemption and my favorite food is mussels and fries!")
+
+#This defines our main function function for our program
+def main():
+	valdescription()
+
+#This will make sure this program is executed first
+if __name__=="__main__":
+	main()
