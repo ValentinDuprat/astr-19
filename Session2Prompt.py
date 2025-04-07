2dcefe00dc111ed11f8ff02f1058b4d6170c0b5b
ValentinDuprat <vrduprat@ucsc.edu>
2025-04-07 00:41:57 -0700

    Session2Prompt

---
+++ Session2Prompt.py
@@ -0,0 +1,16 @@
+#Sum of two floating point numbers
+x = 10.5
+y = 11.1
+print("x+y = ",x+y)
+
+#Define the difference between 2 integers
+x = 2
+y = 3
+print("x-y = ",x-y)
+
+#Product of a floating point number and an integer
+x = 3.5
+y = 2
+print("x*y = ",x*y)
+
+
